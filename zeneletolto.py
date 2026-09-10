05e487f07d5ac7df5855a4fc26a60298cdfcab50
egonixamigod <csokinix@gmail.com>
2026-09-10 16:56:41 +0200

    Release: Build 14 (Auto-Build)

--- zeneletolto.py
+++ zeneletolto.py
@@ -26,7 +26,7 @@ import urllib.error
 import zipfile
 import shutil
 
-BUILD_SZAM = 13                 # a rebuild szkript növeli minden kiadásnál
+BUILD_SZAM = 14                 # a rebuild szkript növeli minden kiadásnál
 PROGRAM_NEV = "YouTube Letöltő"
 
 APP_MAPPA = os.path.join(os.getenv("LOCALAPPDATA", "."), "ZeneLetolto")
